759d02e8891be5f84e2eadd3232d4adf62a1745f
KarsaOrlong1981 <69248804+KarsaOrlong1981@users.noreply.github.com>
2021-11-04 12:26:44 +0100

    Begin to create the PlayerCard, the pics are Dummys and the colors must be later set to Club Colors.

--- FootballManager2021/TeamView.xaml.cs
+++ FootballManager2021/TeamView.xaml.cs
@@ -22,7 +22,7 @@ namespace FootballManager2021
     {
         private List<Club> league;
         CreatClubs creat;
-        public TeamView()
+        public TeamView()// diesem konstrukor muss ich die beiden Teams übergeben,,,,später
         {
             InitializeComponent();
             creat = new CreatClubs();
@@ -66,7 +66,61 @@ namespace FootballManager2021
         private void listName_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
            var name = (sender as ListView).SelectedItem ;
+            allPlayersGrid.Visibility = Visibility.Collapsed;
+            PlayerCard(Convert.ToString(name));
             Debug.WriteLine("Der geklickte Name: " + name);
+        }
+        void PlayerCard(string playerName)
+        {
+            Grid gridDynamic = new Grid
+            {
+               HorizontalAlignment = HorizontalAlignment.Stretch,
+               VerticalAlignment = VerticalAlignment.Stretch,
+               ShowGridLines = true
+            };
+            ColumnDefinition col1 = new ColumnDefinition();
+            col1.Width = new GridLength(1, GridUnitType.Star);
+            ColumnDefinition col2 = new ColumnDefinition();
+            col1.Width = new GridLength(1, GridUnitType.Star);
+            RowDefinition row1 = new RowDefinition();
+            row1.Height = new GridLength(1, GridUnitType.Auto);
+            RowDefinition row2 = new RowDefinition();
+            row2.Height = new GridLength(1, GridUnitType.Auto);
+            gridDynamic.ColumnDefinitions.Add(col1);
+            gridDynamic.ColumnDefinitions.Add(col2);
+            gridDynamic.RowDefinitions.Add(row1);
+            gridDynamic.RowDefinitions.Add(row2);
+
+            Image playerIMG = new Image
+            {
+                Source = new BitmapImage(new Uri("pics/girl30.png", UriKind.Relative)),
+                HorizontalAlignment = HorizontalAlignment.Left,
+                Width = 30,
+                Height = 30,
+                Margin = new Thickness(20)
+                
+            };
+            Grid.SetColumn(playerIMG, 0);
+            Grid.SetRow(playerIMG, 0);
+            Label name = new Label
+            {
+                Content = playerName,
+                FontSize = 25.0,
+                Background = Brushes.Black,
+                Foreground = Brushes.White,
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Stretch,
+                HorizontalContentAlignment = HorizontalAlignment.Center,
+                VerticalContentAlignment = VerticalAlignment.Center
+            };
+           
+            Grid.SetColumn(name, 0);
+            Grid.SetRow(name, 0);
+           
+           
+            gridDynamic.Children.Add(name);
+            gridDynamic.Children.Add(playerIMG);
+            playerCardGrid.Children.Add(gridDynamic);
         }
     }
 }
